77fd6c89c89d5d331e0c6f7d40c79bf5c6193e76
Jeroen Berkvens <Account@Moridrin.com>
2017-07-24 22:47:32 +0200

    Building Parsing seems to be working.

--- Wizardawn/Models/Building.php
+++ Wizardawn/Models/Building.php
@@ -102,7 +102,7 @@ class Building extends JsonObject
     {
         ob_start();
         ?>
-        <table style="position: relative; display: inline-block; border: 1px solid black; margin-right: 4px;">
+        <table style="position: relative; display: inline-block; border: 1px solid black; margin-right: 4px; width: 330px;">
             <tbody>
             <tr>
                 <td><label>Save</label></td>
@@ -114,80 +114,154 @@ class Building extends JsonObject
             <tr>
                 <td><label>Title</label></td>
                 <td>
-                    <input name="building___title[<?= $this->id ?>]" value="<?= $this->title ?>" title="Title">
+                    <input name="building___title[<?= $this->id ?>]" value="<?= $this->title ?>" title="Title" style="width: 100%;">
                 </td>
             </tr>
             <tr>
                 <td><label>Label</label></td>
                 <td>
-                    <input name="building___label[<?= $this->id ?>]" value="<?= $this->label ?>" title="Label">
+                    <input name="building___label[<?= $this->id ?>]" value="<?= $this->label ?>" title="Label" style="width: 100%;">
                 </td>
             </tr>
             <tr>
                 <td><label>NPCs</label></td>
                 <td>
-                    <select name="building___npcs[<?= $this->id ?>][]" title="NPCs" multiple>
+                    <select name="building___npcs[<?= $this->id ?>][]" title="NPCs" multiple style="width: 100%;">
                         <?php
                         global $wpdb;
                         $sql     = "SELECT ID, post_title FROM $wpdb->posts WHERE (post_type = 'area' OR post_type = 'npc' OR post_type = 'item') AND post_status = 'publish'";
                         $objects = $wpdb->get_results($sql);
                         foreach ($objects as $object) {
-                            ?><option value="<?= $object->ID ?>" <?= in_array($object->ID, $this->npcs) ? 'selected' : '' ?>><?= $object->post_title ?></option><?php
+                            ?>
+                            <option value="<?= $object->ID ?>" <?= in_array($object->ID, $this->npcs) ? 'selected' : '' ?>><?= $object->post_title ?></option><?php
                         }
                         ?>
                     </select>
                 </td>
             </tr>
-            <td>
+            <tr>
                 <td><label>Products</label></td>
                 <td>
-                    <table>
+                    <table style="width: 100%;">
+                        <tbody id="<?= $this->id ?>_products_table" style="display: block; height: 150px; overflow-y: auto; overflow-x: hidden;">
+                        <?php $productID = 0; ?>
                         <?php foreach ($this->products as $product): ?>
-                            <tr>
-                                <td><input name="building___products[<?= $this->id ?>][name][]" value="<?= $product->name ?>" title="NPCs"></td>
-                                <td><input name="building___products[<?= $this->id ?>][cost][]" value="<?= $product->cost ?>" title="NPCs"></td>
-                                <td><input name="building___products[<?= $this->id ?>][in_stock][]" value="<?= $product->inStock ?>" title="NPCs"></td>
+                            <tr id="<?= $this->id ?>_product_row_<?= $productID ?>">
+                                <td><input name="building___products[<?= $this->id ?>][name][]" value="<?= $product->name ?>" title="NPCs" style="width: 120px;"></td>
+                                <td><input name="building___products[<?= $this->id ?>][cost][]" value="<?= $product->cost ?>" title="NPCs" style="width: 40px;"></td>
+                                <td><input name="building___products[<?= $this->id ?>][in_stock][]" value="<?= $product->inStock ?>" title="NPCs" style="width: 30px;"></td>
+                                <td><button type="button" onclick="removeProduct('<?= $this->id ?>', '<?= $productID ?>')">X</button></td>
                             </tr>
+                            <?php ++$productID; ?>
                         <?php endforeach; ?>
+                        </tbody>
                     </table>
-                </td>
-            </tr>
-            <td>
-                <td><label>Spells</label></td>
-                <td>
-                    <table>
-                        <?php foreach ($this->spells as $spell): ?>
-                            <tr>
-                                <td><input name="building___spells[<?= $this->id ?>][spell][]" value="<?= $spell->spell ?>" title="Spell name"></td>
-                                <td><input name="building___spells[<?= $this->id ?>][cost][]" value="<?= $spell->cost ?>" title="Spell cost"></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </table>
+                    <button type="button" onclick="addProduct('<?= $this->id ?>', this)" data-rows="<?= $productID ?>">Add</button>
                 </td>
             </tr>
             <tr>
                 <td><label>Spells</label></td>
                 <td>
-                    <select name="building___spells[<?= $this->id ?>][]" title="NPCs" multiple>
-                        <?php
-                        global $wpdb;
-                        $sql     = "SELECT ID, post_title FROM $wpdb->posts WHERE (post_type = 'area' OR post_type = 'npc' OR post_type = 'item') AND post_status = 'publish'";
-                        $objects = $wpdb->get_results($sql);
-                        foreach ($objects as $object) {
-                            ?><option value="<?= $object->ID ?>" <?= in_array($object->ID, $this->npcs) ? 'selected' : '' ?>><?= $object->post_title ?></option><?php
-                        }
-                        ?>
-                    </select>
+                    <table style="width: 100%;">
+                        <tbody id="<?= $this->id ?>_spells_table" style="display: block; height: 150px; overflow-y: auto; overflow-x: hidden;">
+                        <?php $spellID = 0; ?>
+                        <?php foreach ($this->spells as $spell): ?>
+                            <tr id="<?= $this->id ?>_spell_row_<?= $spellID ?>">
+                                <td><input name="building___spells[<?= $this->id ?>][spell][]" value="<?= $spell->spell ?>" title="Spell name" style="width: 150px"></td>
+                                <td><input name="building___spells[<?= $this->id ?>][cost][]" value="<?= $spell->cost ?>" title="Spell cost" style="width: 50px"></td>
+                                <td><button type="button" onclick="removeSpell('<?= $this->id ?>', '<?= $spellID ?>')">X</button></td>
+                            </tr>
+                            <?php ++$spellID; ?>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                    <button type="button" onclick="addSpell('<?= $this->id ?>', this)" data-rows="<?= $spellID ?>">Add</button>
                 </td>
             </tr>
             <tr>
                 <td><label>Type</label></td>
                 <td>
-                    <input name="building___type[<?= $this->id ?>]" value="<?= $this->type ?>" title="Type">
+                    <input name="building___type[<?= $this->id ?>]" value="<?= $this->type ?>" title="Type" style="width: 100%;">
                 </td>
             </tr>
             </tbody>
         </table>
+        <script>
+            if (typeof removeProduct !== 'function') {
+                function removeProduct(buildingID, productID) {
+                    var row = document.getElementById(buildingID + '_product_row_' + productID);
+                    row.parentElement.removeChild(row);
+                }
+
+                function addProduct(buildingID, sender) {
+                    var productID = sender.getAttribute('data-rows');
+                    var row = document.createElement('tr');
+                    row.setAttribute('id', buildingID + '_product_row_' + productID);
+                    var tdName = document.createElement('td');
+                    var inputName = document.createElement('input');
+                    inputName.setAttribute('name', 'building___products[' + buildingID + '][name][]');
+                    inputName.setAttribute('style', 'width: 120px;');
+                    tdName.appendChild(inputName);
+                    row.appendChild(tdName);
+                    var tdCost = document.createElement('td');
+                    var inputCost = document.createElement('input');
+                    inputCost.setAttribute('name', 'building___products[' + buildingID + '][cost][]');
+                    inputCost.setAttribute('style', 'width: 40px;');
+                    tdCost.appendChild(inputCost);
+                    row.appendChild(tdCost);
+                    var tdInStock = document.createElement('td');
+                    var inputInStoc = document.createElement('input');
+                    inputInStoc.setAttribute('name', 'building___products[' + buildingID + '][in_stock][]');
+                    inputInStoc.setAttribute('style', 'width: 30px;');
+                    tdInStock.appendChild(inputInStoc);
+                    row.appendChild(tdInStock);
+                    var tdRemove = document.createElement('td');
+                    var buttonRemove = document.createElement('button');
+                    buttonRemove.setAttribute('type', 'button');
+                    buttonRemove.setAttribute('onclick', 'removeProduct(\'' + buildingID + '\', \'' + productID + '\')');
+                    buttonRemove.innerHTML = 'X';
+                    tdRemove.appendChild(buttonRemove);
+                    row.appendChild(tdRemove);
+                    document.getElementById(buildingID + '_products_table').appendChild(row);
+                    productID++;
+                    sender.setAttribute('data-rows', productID);
+                }
+
+                function removeSpell(buildingID, spellID) {
+                    var row = document.getElementById(buildingID + '_spell_row_' + spellID);
+                    row.parentElement.removeChild(row);
+                }
+
+
+                function addSpell(buildingID, sender) {
+                    var spellID = sender.getAttribute('data-rows');
+                    var row = document.createElement('tr');
+                    row.setAttribute('id', buildingID + '_spell_row_' + spellID);
+                    var tdName = document.createElement('td');
+                    var inputName = document.createElement('input');
+                    inputName.setAttribute('name', 'building___spells[' + buildingID + '][spell][]');
+                    inputName.setAttribute('style', 'width: 150px;');
+                    tdName.appendChild(inputName);
+                    row.appendChild(tdName);
+                    var tdCost = document.createElement('td');
+                    var inputCost = document.createElement('input');
+                    inputCost.setAttribute('name', 'building___spells[' + buildingID + '][cost][]');
+                    inputCost.setAttribute('style', 'width: 50px;');
+                    tdCost.appendChild(inputCost);
+                    row.appendChild(tdCost);
+                    var tdRemove = document.createElement('td');
+                    var buttonRemove = document.createElement('button');
+                    buttonRemove.setAttribute('type', 'button');
+                    buttonRemove.setAttribute('onclick', 'removeSpell(\'' + buildingID + '\', \'' + spellID + '\')');
+                    buttonRemove.innerHTML = 'X';
+                    tdRemove.appendChild(buttonRemove);
+                    row.appendChild(tdRemove);
+                    document.getElementById(buildingID + '_spells_table').appendChild(row);
+                    spellID++;
+                    sender.setAttribute('data-rows', spellID);
+                }
+            }
+        </script>
         <?php
         return ob_get_clean();
     }
@@ -195,19 +269,22 @@ class Building extends JsonObject
     public static function getFromPOST($id, $unset = false)
     {
         $building = new self($_POST['building___label'][$id], $_POST['building___type'][$id]);
-        $fields = [
+        $fields   = [
             'title',
             'npcs',
             'products',
             'spells',
         ];
         foreach ($fields as $field) {
-            $value = $_POST['building___'.$field][$id];
-            if (!empty($_POST['building___'.$field][$id])) {
+            if (!isset($_POST['building___' . $field]) || !isset($_POST['building___' . $field][$id])) {
+                continue;
+            }
+            $value = $_POST['building___' . $field][$id];
+            if (!empty($_POST['building___' . $field][$id])) {
                 $building->$field = $value;
             }
             if ($unset) {
-                unset($_POST['building___'.$field][$id]);
+                unset($_POST['building___' . $field][$id]);
             }
         }
         mp_var_export($building, true);
@@ -219,12 +296,12 @@ class Building extends JsonObject
      */
     public function toWordPress()
     {
-        $title = $this->title;
+        $title   = $this->title;
         $content = $this->description;
 
         /** @var \wpdb $wpdb */
         global $wpdb;
-        $sql = "SELECT p.ID FROM $wpdb->posts AS p";
+        $sql         = "SELECT p.ID FROM $wpdb->posts AS p";
         $keysToCheck = ['height', 'weight'];
         foreach ($keysToCheck as $key) {
             $sql .= " LEFT JOIN $wpdb->postmeta AS pm_$key ON pm_$key.post_id = p.ID";
@@ -232,7 +309,7 @@ class Building extends JsonObject
         $sql .= " WHERE p.post_type = 'npc' AND p.post_title = '$title' AND p.post_content = '$content'";
         foreach ($keysToCheck as $key) {
             $value = $this->$key;
-            $sql .= " AND pm_$key.meta_key = '$key' AND pm_$key.meta_value = '$value'";
+            $sql   .= " AND pm_$key.meta_key = '$key' AND pm_$key.meta_value = '$value'";
         }
         /** @var \WP_Post $foundNPC */
         $foundNPC = $wpdb->get_row($sql);
